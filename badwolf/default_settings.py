3b21308a3e9538bdd1a2c83bbc2cd4d6f3c63aa2
messense <messense@icloud.com>
2015-12-31 15:03:16 +0800

    Update Celery configuration

--- badwolf/default_settings.py
+++ badwolf/default_settings.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from kombu import Exchange, Queue
 
 # is debugging
 DEBUG = False
@@ -14,3 +15,8 @@ SENTRY_DSN = ''
 
 # Celery
 CELERY_BROKER_URL = 'amqp://guest@localhost//'
+CELERY_ACCEPT_CONTENT = ['pickle', 'json', 'msgpack', 'yaml']
+CELERY_QUEUES = (
+    Queue('badwolf', Exchange('badwolf'), routing_key=''),
+)
+CELERY_DEFAULT_QUEUE = 'badwolf'
