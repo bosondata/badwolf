a77b5f902143e60a618cda0cb4e7364074ab8b3f
messense <messense@icloud.com>
2016-01-06 12:23:33 +0800

    Update development environment conf. [ci skip]

--- conf/develop.py
+++ conf/develop.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
-MAIL_SERVER = 'smtp.exmail.qq.com'
+MAIL_SERVER = 'hwsmtp.exmail.qq.com'
 MAIL_PORT = 465
 MAIL_USE_TLS = False
 MAIL_USE_SSL = True
-MAIL_USERNAME = "no-reply@riskstorm.com"
-MAIL_PASSWORD = "bosondata2013"
-MAIL_DEFAULT_SENDER = ('badwolf', 'no-reply@riskstorm.com')
+MAIL_USERNAME = "no-reply@bosondata.com.cn"
+MAIL_PASSWORD = "bosondata2014"
+MAIL_DEFAULT_SENDER = ('badwolf', 'no-reply@bosondata.com.cn')
+MAIL_MAX_EMAILS = None
+
+# SENTRY_DSN = 'http://0eba978d7ab141d8a805ed83242f4c26:98810b9994664ceda91fa6f28619ca44@sentry.bosondata.net/16'
