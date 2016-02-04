f4bce94090e7d45cb8d2286bb8d4f9e79d85de64
messense <messense@icloud.com>
2016-02-04 10:11:54 +0800

    Fix flake8 E402 for badwolf/extensions.py

--- badwolf/extensions.py
+++ badwolf/extensions.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 from raven.contrib.flask import Sentry
-sentry = Sentry()
-
 from flask_mail import Mail
-mail = Mail()
 
 from badwolf.bitbucket import FlaskBitbucket
 
+
+# Sentry
+sentry = Sentry()
+
+# Flask-Mail
+mail = Mail()
+
+# Bitbucket API
 bitbucket = FlaskBitbucket()
