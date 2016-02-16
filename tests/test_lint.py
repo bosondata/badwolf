e1f31be91c4b15f1e97cb0b68d1834a609e18ff0
messense <messense@icloud.com>
2016-02-16 12:27:43 +0800

    Add jscs linter test case. [ci rebuild]

--- tests/test_lint.py
+++ tests/test_lint.py
@@ -134,3 +134,55 @@ index 0000000..fdeea15
     problem = lint.problems[0]
     assert problem.filename == 'a.py'
     assert problem.line == 6
+
+
+def test_jscs_lint_a_js(app, caplog):
+    diff = """diff --git a/.jscsrc b/.jscsrc
+new file mode 100644
+index 0000000..c287019
+--- /dev/null
++++ b/.jscsrc
+@@ -0,0 +1,3 @@
++{
++       "preset": "node-style-guide"
++}
+\ No newline at end of file
+diff --git a/jscs/a.js b/a.js
+new file mode 100644
+index 0000000..66f319a
+--- /dev/null
++++ b/a.js
+@@ -0,0 +1,2 @@
++var foo = 'bar';
++if(foo  === 'bar') {}
+"""
+
+    context = TestContext(
+        'deepanalyzer/badwolf',
+        'git@bitbucket.org:deepanalyzer/badwolf.git',
+        None,
+        'pullrequest',
+        'message',
+        {'commit': {'hash': '000000'}},
+        {'commit': {'hash': '111111'}},
+        pr_id=1
+    )
+    spec = Specification()
+    spec.linters.append('jscs')
+    lint = LintProcessor(context, spec, os.path.join(FIXTURES_PATH, 'jscs'))
+    patch = PatchSet(diff.split('\n'))
+    with mock.patch.object(lint, 'load_changes') as load_changes,\
+            mock.patch.object(lint, 'update_build_status') as build_status,\
+            mock.patch.object(lint, '_report') as report:
+        load_changes.return_value = patch
+        build_status.return_value = None
+        report.return_value = None
+        lint.problems.set_changes(patch)
+        lint.process()
+
+        assert load_changes.called
+
+    assert len(lint.problems) == 1
+    problem = lint.problems[0]
+    assert problem.filename == 'a.js'
+    assert problem.line == 2
