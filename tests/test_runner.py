298b85a7c36e536a985b7ccffc8fefa135baa187
messense <messense@icloud.com>
2016-02-19 16:46:33 +0800

    Fix TestRunner test case

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+import os
+import tempfile
 from multiprocessing import Lock
 
 import git
@@ -7,6 +9,7 @@ import mock
 import pytest
 
 from badwolf.runner import TestContext, TestRunner
+from badwolf.bitbucket import PullRequest, Changesets
 
 
 @pytest.fixture(scope='function')
@@ -26,15 +29,27 @@ def push_context():
 
 @pytest.fixture(scope='function')
 def push_runner(push_context):
-    return TestRunner(push_context, Lock())
+    runner = TestRunner(push_context, Lock())
+    runner.clone_path = os.path.join(
+        tempfile.gettempdir(),
+        'badwolf',
+        runner.task_id,
+        runner.repo_name
+    )
+    return runner
 
 
 def test_clone_repo_failed(app, push_runner):
     with mock.patch.object(push_runner, 'update_build_status') as status, \
             mock.patch.object(push_runner, 'clone_repository') as clone_repo, \
-            mock.patch.object(push_runner, 'validate_settings') as validate_settings:
+            mock.patch.object(push_runner, 'validate_settings') as validate_settings, \
+            mock.patch.object(PullRequest, 'comment') as pr_comment, \
+            mock.patch.object(Changesets, 'comment') as cs_comment:
         status.return_value = None
         clone_repo.side_effect = git.GitCommandError('git clone', 1)
+        pr_comment.return_value = None
+        cs_comment.return_value = None
+
         push_runner.run()
 
         validate_settings.assert_not_called()
