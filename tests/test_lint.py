d0ab66ca2ea3585626a5fefa18914c2533b236bc
messense <messense@icloud.com>
2016-02-19 14:12:26 +0800

    Support multiple glob patterns in linter configuration

--- tests/test_lint.py
+++ tests/test_lint.py
@@ -544,3 +544,49 @@ index 0000000..1eccee8
     problem = lint.problems[0]
     assert problem.filename == 'a.yml'
     assert problem.line == 3
+
+
+def test_flake8_lint_a_py_with_multi_custom_glob_patterns(app, caplog):
+    diff = """diff --git a/b.pyx b/b.pyx
+new file mode 100644
+index 0000000..fdeea15
+--- /dev/null
++++ b/b.pyx
+@@ -0,0 +1,6 @@
++# -*- coding: utf-8 -*-
++from __future__ import absolute_import, unicode_literals
++
++
++def add(a, b):
++    return a+ b
+"""
+
+    context = TestContext(
+        'deepanalyzer/badwolf',
+        'git@bitbucket.org:deepanalyzer/badwolf.git',
+        None,
+        'pullrequest',
+        'message',
+        {'commit': {'hash': '000000'}},
+        {'commit': {'hash': '111111'}},
+        pr_id=1
+    )
+    spec = Specification()
+    spec.linters.append(ObjectDict(name='flake8', pattern='*.py *.pyx'))
+    lint = LintProcessor(context, spec, os.path.join(FIXTURES_PATH, 'flake8'))
+    patch = PatchSet(diff.split('\n'))
+    with mock.patch.object(lint, 'load_changes') as load_changes,\
+            mock.patch.object(lint, 'update_build_status') as build_status,\
+            mock.patch.object(lint, '_report') as report:
+        load_changes.return_value = patch
+        build_status.return_value = None
+        report.return_value = None
+        lint.problems.set_changes(patch)
+        lint.process()
+
+        assert load_changes.called
+
+    assert len(lint.problems) == 1
+    problem = lint.problems[0]
+    assert problem.filename == 'b.pyx'
+    assert problem.line == 6
