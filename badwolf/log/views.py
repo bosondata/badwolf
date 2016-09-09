48cee90abdb1687a9a84440f586964615b38889c
messense <messense@icloud.com>
2016-09-09 10:28:12 +0800

    Use from.send_from_directory instead

--- badwolf/log/views.py
+++ badwolf/log/views.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 import logging
 
-from flask import Blueprint, current_app, abort
+from flask import Blueprint, current_app, send_from_directory
 
 
 logger = logging.getLogger(__name__)
@@ -13,20 +13,10 @@ blueprint = Blueprint('log', __name__)
 @blueprint.route('/build/<sha>', methods=['GET'])
 def build_log(sha):
     log_dir = os.path.join(current_app.config['BADWOLF_LOG_DIR'], sha)
-    log_html = os.path.join(log_dir, 'build.html')
-    if not os.path.exists(log_html):
-        abort(404)
-
-    with open(log_html) as f:
-        return f.read()
+    return send_from_directory(log_dir, 'build.html')
 
 
 @blueprint.route('/lint/<sha>', methods=['GET'])
 def lint_log(sha):
     log_dir = os.path.join(current_app.config['BADWOLF_LOG_DIR'], sha)
-    log_html = os.path.join(log_dir, 'lint.html')
-    if not os.path.exists(log_html):
-        abort(404)
-
-    with open(log_html) as f:
-        return f.read()
+    return send_from_directory(log_dir, 'lint.html')
