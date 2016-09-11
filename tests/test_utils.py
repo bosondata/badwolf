18808b6594d7e2b1c81a2cf4351708e179fb29bb
messense <messense@icloud.com>
2016-09-11 15:32:31 +0800

    Update test case for sanitize_sensitive_data

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -32,3 +32,12 @@ def test_sanitize_basic_auth_urls():
     assert 'pwd' not in sanitized
     assert 'http://example.com' in sanitized
     assert 'git+https://***:***@example.com' in sanitized
+
+    lots_of_urls = ['-e git+https://user:pwd@example.com  abcd'] * 1000
+    lots_of_urls.extend(['abc http://example.com def'] * 1000)
+    text = '\n'.join(lots_of_urls)
+    sanitized = sanitize_sensitive_data(text)
+    assert 'user' not in sanitized
+    assert 'pwd' not in sanitized
+    assert 'http://example.com' in sanitized
+    assert 'git+https://***:***@example.com' in sanitized
