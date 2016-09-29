3f444fb4c85bd0867c521d1022d5eb0e8a15f632
messense <messense@icloud.com>
2016-09-29 23:20:41 +0800

    Fully test cover auto merge pull requests

--- tests/test_auto_merge_pull_request.py
+++ tests/test_auto_merge_pull_request.py
@@ -13,7 +13,16 @@ import badwolf.bitbucket as bitbucket
 
 def test_auto_merge_not_enabled(app, test_client):
     app.config['AUTO_MERGE_ENABLED'] = False
-    payload = '{}'
+    payload = json.dumps({
+        'repository': {
+            'full_name': 'deepanalyzer/badwolf',
+        },
+        'pullrequest': {
+            'id': 1,
+            'title': 'PR 1',
+            'description': 'This is PR1',
+        },
+    })
     with mock.patch.object(bitbucket.PullRequest, 'get') as pr_get:
         test_client.post(
             url_for('webhook.webhook_push'),
@@ -24,6 +33,30 @@ def test_auto_merge_not_enabled(app, test_client):
         )
         pr_get.assert_not_called()
     app.config['AUTO_MERGE_ENABLED'] = True
+
+
+def test_auto_merge_failure_pr_get_error(app, test_client):
+    payload = json.dumps({
+        'repository': {
+            'full_name': 'deepanalyzer/badwolf',
+        },
+        'pullrequest': {
+            'id': 1,
+            'title': 'PR 1',
+            'description': 'This is PR1',
+        },
+    })
+    with mock.patch.object(bitbucket.PullRequest, 'get') as pr_get:
+        pr_get.side_effect = bitbucket.BitbucketAPIError(404, 'not found', 'PR not found')
+        test_client.post(
+            url_for('webhook.webhook_push'),
+            data=payload,
+            headers={
+                'X-Event-Key': 'pullrequest:approved',
+            }
+        )
+        with mock.patch.object(bitbucket.BuildStatus, 'get') as status_get:
+            status_get.assert_not_called()
 
 
 def test_auto_merge_skip_title(test_client):
@@ -94,3 +127,116 @@ def test_auto_merge_skip_pr_not_in_open_state(test_client):
         )
         with mock.patch.object(bitbucket.BuildStatus, 'get') as status_get:
             status_get.assert_not_called()
+
+
+def test_auto_merge_skip_not_enough_approval(test_client):
+    payload = json.dumps({
+        'repository': {
+            'full_name': 'deepanalyzer/badwolf',
+        },
+        'pullrequest': {
+            'id': 1,
+            'title': 'PR 1',
+            'description': 'This is PR1',
+        },
+    })
+    with mock.patch.object(bitbucket.PullRequest, 'get') as pr_get:
+        pr_get.return_value = {
+            'state': 'OPEN',
+            'participants': [
+                {'approved': False},
+                {'approved': True},
+                {'approved': False},
+            ]
+        }
+        test_client.post(
+            url_for('webhook.webhook_push'),
+            data=payload,
+            headers={
+                'X-Event-Key': 'pullrequest:approved',
+            }
+        )
+        with mock.patch.object(bitbucket.BuildStatus, 'get') as status_get:
+            status_get.assert_not_called()
+
+
+def test_auto_merge_success(test_client):
+    payload = json.dumps({
+        'repository': {
+            'full_name': 'deepanalyzer/badwolf',
+        },
+        'pullrequest': {
+            'id': 1,
+            'title': 'PR 1',
+            'description': 'This is PR1',
+        },
+    })
+    with mock.patch.object(bitbucket.PullRequest, 'get') as pr_get, \
+            mock.patch.object(bitbucket.PullRequest, 'merge') as pr_merge, \
+            mock.patch.object(bitbucket.BuildStatus, 'get') as status_get:
+        pr_get.return_value = {
+            'state': 'OPEN',
+            'participants': [
+                {'approved': True},
+                {'approved': True},
+                {'approved': True},
+            ],
+            'source': {
+                'repository': {'full_name': 'deepanalyzer/badwolf'},
+                'commit': {'hash': '0000000'},
+            },
+        }
+        status_get.return_value = {
+            'state': 'SUCCESSFUL',
+        }
+        pr_merge.return_value = None
+        test_client.post(
+            url_for('webhook.webhook_push'),
+            data=payload,
+            headers={
+                'X-Event-Key': 'pullrequest:approved',
+            }
+        )
+        assert status_get.called
+        assert pr_merge.called
+
+
+def test_auto_merge_call_error(test_client):
+    payload = json.dumps({
+        'repository': {
+            'full_name': 'deepanalyzer/badwolf',
+        },
+        'pullrequest': {
+            'id': 1,
+            'title': 'PR 1',
+            'description': 'This is PR1',
+        },
+    })
+    with mock.patch.object(bitbucket.PullRequest, 'get') as pr_get, \
+            mock.patch.object(bitbucket.PullRequest, 'merge') as pr_merge, \
+            mock.patch.object(bitbucket.BuildStatus, 'get') as status_get:
+        pr_get.return_value = {
+            'state': 'OPEN',
+            'participants': [
+                {'approved': True},
+                {'approved': True},
+                {'approved': True},
+            ],
+            'source': {
+                'repository': {'full_name': 'deepanalyzer/badwolf'},
+                'commit': {'hash': '0000000'},
+            },
+        }
+        status_get.return_value = {
+            'state': 'SUCCESSFUL',
+        }
+        pr_merge.side_effect = bitbucket.BitbucketAPIError(401, 'access denied', 'access denied')
+        test_client.post(
+            url_for('webhook.webhook_push'),
+            data=payload,
+            headers={
+                'X-Event-Key': 'pullrequest:approved',
+            }
+        )
+        assert status_get.called
+        assert pr_merge.called
