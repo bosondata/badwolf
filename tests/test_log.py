690c08b2b35df2d81dc0977d8bd593c45806e1c2
messense <messense@icloud.com>
2016-09-29 23:24:37 +0800

    Add dumb log view test cases

---
+++ tests/test_log.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+from flask import url_for
+
+
+def test_view_build_log(test_client):
+    test_client.get(url_for('log.build_log', sha='123456'))
+
+
+def test_view_lint_log(test_client):
+    test_client.get(url_for('log.lint_log', sha='123456'))
