4822753091037c71240e929ec92f82d8010a473a
messense <messense@icloud.com>
2016-12-20 11:14:54 +0800

    Also ignore minified CSS code when lint

--- badwolf/lint/linters/csslint.py
+++ badwolf/lint/linters/csslint.py
@@ -14,6 +14,12 @@ class CSSLinter(Linter):
     def is_usable(self):
         return in_path('csslint') or npm_exists('csslint', self.working_dir)
 
+    def match_file(self, filename):
+        # 过滤掉压缩过的 CSS 文件
+        if filename.lower().endswith('.min.css'):
+            return False
+        return super(CSSLinter, self).match_file(filename)
+
     def lint_files(self, files):
         command = self.create_command(files)
         _, output = run_command(command, cwd=self.working_dir)
--- badwolf/lint/linters/stylelint.py
+++ badwolf/lint/linters/stylelint.py
@@ -15,6 +15,12 @@ class StyleLinter(Linter):
     def is_usable(self):
         return in_path('stylelint') or npm_exists('stylelint', self.working_dir)
 
+    def match_file(self, filename):
+        # 过滤掉压缩过的 CSS 文件
+        if filename.lower().endswith('.min.css'):
+            return False
+        return super(StyleLinter, self).match_file(filename)
+
     def lint_files(self, files):
         command = self.create_command(files)
         _, output = run_command(command, cwd=self.working_dir)
