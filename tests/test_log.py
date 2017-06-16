0ccb85a45e56438a5e4b7c0566634587624f0ce4
messense <messense@icloud.com>
2017-06-16 16:10:53 +0800

    Remove lint_log view test case

--- tests/test_log.py
+++ tests/test_log.py
@@ -4,7 +4,3 @@ from flask import url_for
 
 def test_view_build_log(test_client):
     test_client.get(url_for('log.build_log', sha='123456'))
-
-
-def test_view_lint_log(test_client):
-    test_client.get(url_for('log.lint_log', sha='123456'))
