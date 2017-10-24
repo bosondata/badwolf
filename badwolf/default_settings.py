54702ed1ee916dad567e13b65823c87901f9ed2d
messense <messense@icloud.com>
2017-10-24 21:57:46 +0800

    Use os.getenv instead of os.environ.get

--- badwolf/default_settings.py
+++ badwolf/default_settings.py
@@ -11,36 +11,36 @@ import deansi
 from badwolf.utils import yesish
 
 # is debugging
-DEBUG = yesish(os.environ.get('BADWOLF_DEBUG', False))
+DEBUG = yesish(os.getenv('BADWOLF_DEBUG', False))
 
-JSON_AS_ASCII = yesish(os.environ.get('JSON_AS_ASCII', False))
-SERVER_NAME = os.environ.get('SERVER_NAME', 'localhost:8000')
+JSON_AS_ASCII = yesish(os.getenv('JSON_AS_ASCII', False))
+SERVER_NAME = os.getenv('SERVER_NAME', 'localhost:8000')
 
 # secret key
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # secure token key
-SECURE_TOKEN_KEY = os.environ.get('SECURE_TOKEN_KEY', base64.urlsafe_b64encode(os.urandom(32)))
+SECURE_TOKEN_KEY = os.getenv('SECURE_TOKEN_KEY', base64.urlsafe_b64encode(os.urandom(32)))
 
 # Sentry
-SENTRY_DSN = os.environ.get('SENTRY_DSN', '')
+SENTRY_DSN = os.getenv('SENTRY_DSN', '')
 
 # Docker
-DOCKER_HOST = os.environ.get('DOCKER_HOST', 'unix:///var/run/docker.sock')
-DOCKER_API_TIMEOUT = int(os.environ.get('DOCKER_API_TIMEOUT', 600))
-DOCKER_RUN_TIMEOUT = int(os.environ.get('DOCKER_RUN_TIMEOUT', 1200))
+DOCKER_HOST = os.getenv('DOCKER_HOST', 'unix:///var/run/docker.sock')
+DOCKER_API_TIMEOUT = int(os.getenv('DOCKER_API_TIMEOUT', 600))
+DOCKER_RUN_TIMEOUT = int(os.getenv('DOCKER_RUN_TIMEOUT', 1200))
 
 # Mail
-MAIL_SERVER = os.environ.get('MAIL_SERVER', '')
-MAIL_PORT = int(os.environ.get('MAIL_PORT', 587))
-MAIL_USE_TLS = yesish(os.environ.get('MAIL_USE_TLS', True))
-MAIL_USE_SSL = yesish(os.environ.get('MAIL_USE_SSL', False))
-MAIL_USERNAME = os.environ.get('MAIL_USERNAME', '')
-MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', '')
+MAIL_SERVER = os.getenv('MAIL_SERVER', '')
+MAIL_PORT = int(os.getenv('MAIL_PORT', 587))
+MAIL_USE_TLS = yesish(os.getenv('MAIL_USE_TLS', True))
+MAIL_USE_SSL = yesish(os.getenv('MAIL_USE_SSL', False))
+MAIL_USERNAME = os.getenv('MAIL_USERNAME', '')
+MAIL_PASSWORD = os.getenv('MAIL_PASSWORD', '')
 MAIL_MAX_EMAILS = None
 MAIL_DEFAULT_SENDER = (
-    os.environ.get('MAIL_SENDER_NAME', 'badwolf'),
-    os.environ.get('MAIL_SENDER_ADDRESS', '')
+    os.getenv('MAIL_SENDER_NAME', 'badwolf'),
+    os.getenv('MAIL_SENDER_ADDRESS', '')
 )
 
 # Logging
@@ -73,16 +73,16 @@ LOGGING = {
 }
 
 BADWOLF_PROJECT_CONF = '.badwolf.yml'
-AUTO_MERGE_ENABLED = yesish(os.environ.get('AUTO_MERGE_ENABLED', True))
-AUTO_MERGE_APPROVAL_COUNT = int(os.environ.get('AUTO_MERGE_APPROVAL_COUNT', 3))
+AUTO_MERGE_ENABLED = yesish(os.getenv('AUTO_MERGE_ENABLED', True))
+AUTO_MERGE_APPROVAL_COUNT = int(os.getenv('AUTO_MERGE_APPROVAL_COUNT', 3))
 
-BITBUCKET_OAUTH_KEY = os.environ.get('BITBUCKET_OAUTH_KEY', '')
-BITBUCKET_OAUTH_SECRET = os.environ.get('BITBUCKET_OAUTH_SECRET', '')
+BITBUCKET_OAUTH_KEY = os.getenv('BITBUCKET_OAUTH_KEY', '')
+BITBUCKET_OAUTH_SECRET = os.getenv('BITBUCKET_OAUTH_SECRET', '')
 
-BITBUCKET_USERNAME = os.environ.get('BITBUCKET_USERNAME', '')
-BITBUCKET_PASSWORD = os.environ.get('BITBUCKET_PASSWORD', '')
+BITBUCKET_USERNAME = os.getenv('BITBUCKET_USERNAME', '')
+BITBUCKET_PASSWORD = os.getenv('BITBUCKET_PASSWORD', '')
 
-BADWOLF_DATA_DIR = os.environ.get('BADWOLF_DATA_DIR', '/var/lib/badwolf')
+BADWOLF_DATA_DIR = os.getenv('BADWOLF_DATA_DIR', '/var/lib/badwolf')
 if DEBUG:
     if platform.system() == 'Darwin':
         # On macOS, tempfile.gettempdir function doesn't return '/tmp'
@@ -91,9 +91,9 @@ if DEBUG:
         BADWOLF_DATA_DIR = '/tmp/badwolf'  # nosec
     else:
         BADWOLF_DATA_DIR = os.path.join(tempfile.gettempdir(), 'badwolf')
-BADWOLF_LOG_DIR = os.environ.get('BADWOLF_LOG_DIR', os.path.join(BADWOLF_DATA_DIR, 'log'))
-BADWOLF_REPO_DIR = os.environ.get('BADWOLF_REPO_DIR', os.path.join(BADWOLF_DATA_DIR, 'repos'))
-BADWOLF_ARTIFACTS_DIR = os.environ.get('BADWOLF_REPO_DIR', os.path.join(BADWOLF_DATA_DIR, 'artifacts'))
+BADWOLF_LOG_DIR = os.getenv('BADWOLF_LOG_DIR', os.path.join(BADWOLF_DATA_DIR, 'log'))
+BADWOLF_REPO_DIR = os.getenv('BADWOLF_REPO_DIR', os.path.join(BADWOLF_DATA_DIR, 'repos'))
+BADWOLF_ARTIFACTS_DIR = os.getenv('BADWOLF_REPO_DIR', os.path.join(BADWOLF_DATA_DIR, 'artifacts'))
 
 # Sentry Release
 try:
